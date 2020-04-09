f7a1a4fbdd828cbafef4b2df349c11761d4dbd98
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-04-09 13:33:30 -0300

    Remove placeholders due to materialize conflict

--- app/forms.py
+++ app/forms.py
@@ -6,26 +6,25 @@ from locations.models import State
 
 
 class SignupForm(AllauthSignupForm):
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        del self.fields["email"].widget.attrs["placeholder"]
+        del self.fields["password2"].widget.attrs["placeholder"]
+
     cpf = forms.CharField(
         label="CPF",
         min_length=14,
         max_length=14,
-        widget=forms.TextInput(attrs={"data-mask": "000.000.000-00", "placeholder": "CPF"}),
+        widget=forms.TextInput(attrs={"data-mask": "000.000.000-00"}),
         validators=[CPFValidator()],
     )
 
     first_name = forms.CharField(
-        label="Nome",
-        min_length=3,
-        max_length=100,
-        widget=forms.TextInput(attrs={"placeholder": "Nome"}),
+        label="Nome", min_length=3, max_length=100, widget=forms.TextInput()
     )
 
     last_name = forms.CharField(
-        label="Sobrenome",
-        min_length=3,
-        max_length=100,
-        widget=forms.TextInput(attrs={"placeholder": "Sobrenome"}),
+        label="Sobrenome", min_length=3, max_length=100, widget=forms.TextInput()
     )
 
     state = forms.ModelChoiceField(
