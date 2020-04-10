ba77e3399c0218bf9d686cdc675332564beb4a15
Pablo Diego <pablodiegoss@hotmail.com>
2020-04-09 22:28:11 -0300

    Recreate deleted db_fake task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,22 @@ def db(ctx):
 
 
 @task
+def db_fake(ctx, force=False, app=True, users=True):
+    """
+    Adds fake data to the database for development tests
+    """
+    if force or os.environ.get("FAKE_DB") == "true":
+        print("Creating fake data...")
+    else:
+        return print("FAKE_DB is False")
+
+    if users:
+        ctx.run("python manage.py createfakeusers")
+    if app:
+        ctx.run("python manage.py createfakeapp")
+
+
+@task
 def migrate(ctx):
     """Alias to Django's "migrate" command."""
     ctx.run("python manage.py migrate")
