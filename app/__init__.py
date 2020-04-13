1337360e7e5debc3ea219f29e9674c0971c9d608
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-04-13 01:20:45 -0300

    Add capacity plot

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,5 @@
+import project.cheats as _tweaks
+
 default_app_config = "app.apps.AppConfig"
+
+del _tweaks
--- app/views/notifier_views.py
+++ app/views/notifier_views.py
@@ -1,7 +1,13 @@
 from django import forms
+from django.shortcuts import render
 from material.frontend.views import CreateModelView, ListModelView, UpdateModelView
 
-__all__ = ["NotifierListModelView", "NotifierUpdateModelView", "NotifierCreateModelView"]
+__all__ = [
+    "NotifierListModelView",
+    "NotifierUpdateModelView",
+    "NotifierCreateModelView",
+    "notification_history_view",
+]
 
 
 class NotifierCreateOrUpdateMixin:
@@ -42,15 +48,28 @@ class NotifierCreateOrUpdateMixin:
 
 
 class NotifierCreateModelView(NotifierCreateOrUpdateMixin, CreateModelView):
-    pass
+    """
+    Creates a new notification
+    """
 
 
 class NotifierUpdateModelView(NotifierCreateOrUpdateMixin, UpdateModelView):
-    pass
+    """
+    Updates a new notification
+    """
 
 
 class NotifierListModelView(ListModelView):
+    """
+    List notifications.
+    """
+
     def get_queryset(self):
         user = self.request.user
         qs = super().get_queryset()
         return qs.filter(notifier=user)
+
+
+def notification_history_view(request):
+    units = request.user.healthcare_units.all()
+    return render(request, "app/notification_history.html", {"units": units})
