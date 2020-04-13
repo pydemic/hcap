3421b52e038286039bd2a974c27b87f8b32ea391
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-04-12 22:12:44 -0300

    Optimize query in Django admin

--- app/admin.py
+++ app/admin.py
@@ -19,3 +19,6 @@ class HealthcareUnitAdmin(ModelAdmin):
 
     get_city_name.short_description = "Município"
     get_city_name.admin_order_field = "city__name"
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("city")
