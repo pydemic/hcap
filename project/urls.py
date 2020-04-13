59f24a1291145094f7eaf5d7b91c430ff63c3120
Jonathan Henrique Maia de Moraes <altjohndev@gmail.com>
2020-04-12 21:22:56 -0300

    Set grafana dashboard url as setting

--- project/urls.py
+++ project/urls.py
@@ -19,18 +19,9 @@ from django.views.generic import RedirectView
 from material.frontend import urls as frontend_urls
 import environ
 
-
-def get_grafana_url():
-    env = environ.Env()
-    domain = env("GRAFANA_URL", default="localhost:3000")
-    token = env("DASHBOARD_TOKEN", default="OMynCUCWz")
-    general_dashboard = f"http://{domain}/d/{token}/capacidade-hospitalar?orgId=1"
-    return general_dashboard
-
-
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
-    path("dashboard/", RedirectView.as_view(url=get_grafana_url()), name="grafana"),
+    path("dashboard/", RedirectView.as_view(url=settings.GRAFANA_DASHBOARD_URL), name="grafana"),
     path("", RedirectView.as_view(url="./app/"), name="home"),
     path("", include(frontend_urls)),
     path("", include("django_prometheus.urls")),
